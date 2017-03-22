cb3c39eb3804e857f8876b99ebb9cf276d6a91af
7coco <coco.greenery@gmail.com>
2017-03-22 16:39:27 +0900

    Implement Sample3_2

---
+++ Sample3_2.java
@@ -0,0 +1,12 @@
+class Sample3_2
+{
+  public static void main(String[] args)
+  {
+    int num;
+    num = 3;
+    System.out.println("変数 num の値は" + num + "です。");
+    num = 5;
+    System.out.println("変数 num の値を変更しました。");
+    System.out.println("変数 num の新しい値は" + num + "です。");
+  }
+}
