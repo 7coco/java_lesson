fec1eb0353fcaa4882cf4b02577225fbec127f97
7coco <coco.greenery@gmail.com>
2017-03-22 16:46:18 +0900

    Implement Sample3_3

---
+++ Sample3_3.java
@@ -0,0 +1,12 @@
+class Sample3_3
+{
+  public static void main(String[] args)
+  {
+    int num1, num2;
+    num1 = 3;
+    System.out.println("変数 num1 の値は" + num1 + "です。");
+    num2 = num1;
+    System.out.println("変数 num1 の値を変数 num2 に変更しました。");
+    System.out.println("変数 num2 の値は" + num2 + "です。");
+  }
+}
