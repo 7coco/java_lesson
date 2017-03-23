b3a61c426e533bba8de1d42798e287b815ad4fb0
7coco <coco.greenery@gmail.com>
2017-03-23 14:28:50 +0900

    Implement Practice5_4

---
+++ Practice5_4.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+class Practice5_4
+{
+  public static void main(String[] args) throws IOException
+  {
+    System.out.println("0から10までの整数を入力してください");
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    String res = br.readLine();
+    int num = Integer.parseInt(res);
+
+    if(num >= 0 && num <= 10){
+      System.out.println("正解です。");
+    }else{
+      System.out.println("間違いです。");
+    }
+  }
+}
