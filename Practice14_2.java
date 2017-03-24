35561a4659e0f63bfb57a593a55fd289b867b4b4
7coco <coco.greenery@gmail.com>
2017-03-24 10:04:18 +0900

    Implement Practice14_2

---
+++ Practice14_2.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+class Practice14_2
+{
+  public static void main(String[] args) {
+    try{
+      PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("test1.txt")));
+
+      pw.println("A long time ago, ");
+      pw.println("There was a little girl.");
+
+      pw.close();
+    }
+    catch(IOException e){
+      System.out.println("入出力エラーです。");
+    }
+  }
+}
